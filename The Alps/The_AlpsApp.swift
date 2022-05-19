d5cf90bcfcea939f239e3a68e724d5ad31002ad9
Oliver Gorst <oliver.gorst@outlook.com>
2022-05-18 21:06:04 -0400

    Initial Commit

---
+++ The Alps/The_AlpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_AlpsApp.swift
+//  The Alps
+//
+//  Created by Oliver Gorst on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct The_AlpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
